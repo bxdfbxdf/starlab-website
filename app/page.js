04cc52d98cc7091f4846eb991463fdd7ed567422
Morgan Jenks <morgan.m.jenks@gmail.com>
2024-07-30 09:53:02 -0500

    rebuild main

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ import About from '@/components/About'
 import YoutubeGallery from '@/components/YoutubeGallery'
 
 export default function Home() {
-
   return (
     <main>
       <YoutubeEmbed embedId='HgBd14YxVoM'/>
